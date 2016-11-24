6cd0def2d9b919b313edaa3b04091e1920ba5da5
zeyan <258785420@qq.com>
2016-11-24 19:31:47 +0800

    add function diamond

--- test.php
+++ test.php
@@ -3,12 +3,46 @@
 
 function square($num1,$num2)
 {
-  for ($i=0; $i <$num1 ; $i++) {
-    echo "\n";
-    for ($j=0; $j <$num2 ; $j++) {
-      echo "*";
-    }
+    for ($i=0; $i <$num1 ; $i++)
+    {
+        echo "\n";
+          for ($j=0; $j <$num2 ; $j++)
+          {
+              echo "*";
+            }
     }
   }
 
 square(4,6);
+
+function diamond($num1)
+{
+    echo "============空心菱形======================\n";
+    for ($a=1; $a<=$num1 ; $a++)
+      {
+    	for ($b=1; $b <=$num1-$a ; $b++) {
+    		echo " ";
+    	}
+    	for ($c=1; $c<=2*$a-1 ; $c++) {
+    				if($c==1||$c==2*$a-1){
+    					echo "*";
+    				}else {
+    						echo " ";
+    	}
+    	}echo "\n";
+    	}
+    	for ($a=$num1-1; $a>=0 ; $a--) {
+    		for ($b=1; $b <=$num1-$a ; $b++) {
+    			echo " ";
+    		}
+    		for ($c=1; $c<=2*$a-1 ; $c++) {
+    					if($c==1||$c==2*$a-1){
+    						echo "*";
+    					}else {
+    							echo " ";
+    		          }
+    		}echo "\n";
+    	}
+}
+
+diamond(6);
